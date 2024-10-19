41805f511374035eb65514d7439a44a829192236
Aidanjm13 <ajmathu13@outlook.com>
2024-10-19 18:47:28 -0400

    Button response set up

--- SCAVENGR/static/javascript/index.js
+++ SCAVENGR/static/javascript/index.js
@@ -7,6 +7,13 @@ let myLineChart = null;
 //test 3
 
 $(document).ready(function() {
-    
+    $('#submitButton').click(function() {
+        const textBox = document.getElementById("textBox");
+        text = textBox.value;
+        console.log(text);
+        //do the loading symbol
+        //use text to search for regex
+        //display result
+    });
 });
 
